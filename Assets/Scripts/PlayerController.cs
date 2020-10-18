79ca480707cc2b0ce88a28a3f4a0a2b90e0ed342
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-10-18 03:08:59 -0700

    Adding player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb = null;
+    Vector3 direction = Vector3.zero;
+    [SerializeField]
+    float velocity = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //if (gameObject.name == FindObjectOfType<NetworkMan>().myAddress)
+        //{
+        //    active = true;
+        //}
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = velocity * direction;  // calc directional velocity
+    }
+}
